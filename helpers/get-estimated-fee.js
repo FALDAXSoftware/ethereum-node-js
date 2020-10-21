fdd4b13394db2b4a5a8239d2e7bbf0dbbcda44b9
Dharmveersinh <dharmveersinh1999@gmail.com>
2020-10-21 13:46:42 +0530

    Fees,Send ETH,Balance done

---
+++ helpers/get-estimated-fee.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var Web3 = require('web3');
+var abi = require('./abi.json');
+
+var getFee = async () => {
+    var web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_URL));
+    var gasPrice = await web3.eth.getGasPrice();
+    console.log(gasPrice);
+    var fees = gasPrice*36000;
+    console.log(fees);
+    return web3.utils.fromWei(fees.toString(),'ether');
+}   
+
+module.exports = {
+    getFee
+}
